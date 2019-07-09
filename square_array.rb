cc31c6db6d9e1e5a986f79b6110951a64b753ad6
Ryan May <ryan.m.may@outlook.com>
2019-07-09 13:14:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,19 +1,19 @@
+
+def square_array(array)
+  #create placeholder for storing squared numbers
+  sq_arr = []   
+  
+  #send squared numbers to "sq_arr"
+  array.each { |x| sq_arr << x ** 2 }
+  
+  #send output of squared numbers
+  sq_arr               
+end 
+
 =begin
 def square_array(array)
-  
-  sq_arr = []           #create placeholder for storing squared numbers
-  
-  array.each do |x| 
-    sq_arr << x ** 2    #send squared numbers to "arr"
-  end
-  
-  sq_arr                #send output of squared numbers
-end 
-=end
-
-def square_array(array)
   sq_arr = []
-  
   array.collect { |x| sq_arr << x ** 2 }
-
+  sq_arr
 end
+=end
